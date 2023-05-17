b60d4bd8b92518dd03d356b04f436ecc5ad40a2d
Valentin Viovi <vviovi@2F7.42angouleme.fr>
2023-05-17 14:35:13 +0200

    creation src.h

---
+++ sources/includes/src.h
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   src.h                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vviovi <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/17 08:50:20 by vviovi            #+#    #+#             */
+/*   Updated: 2023/05/17 10:32:15 by vviovi           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SRC_H
+# define SRC_H
+
+# include "type.h"
+# include "../../libft/include/libft.h"
+
+# include <fcntl.h>
+# include <stdlib.h>
+# include <unistd.h>
+
+//map_get_info
+int		get_texture_info(int file_fd, char *card, t_data *data, int index_tab);
+int		get_color_info(int file_fd, char place, t_data *data);
+
+//map_verif
+int		valid_extension(char *path, char *extension);
+int		valid_color(int red, int green, int blue);
+int		valid_texture(char *path_texture);
+
+//utils_map
+char	*gnl_skip_void(int fd_file);
+int		len_dbl_tab(char **tab);
+int		print_error_map(int type_error);
+
+//load_map
+int		load_file(char **argv, t_data *data);
+
+#endif
