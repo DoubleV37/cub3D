1c4b181913cc9aec5a0e75f05a9d3e14ba677f77
Valentin Viovi <vviovi@2C2.42angouleme.fr>
2023-06-01 10:58:04 +0200

    implémentation de la fonction pour calculer l'echelle

--- sources/draw/draw_texture.c
+++ sources/draw/draw_texture.c
@@ -6,7 +6,7 @@
 /*   By: vviovi <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/25 13:44:26 by vviovi            #+#    #+#             */
-/*   Updated: 2023/05/30 09:48:50 by vviovi           ###   ########.fr       */
+/*   Updated: 2023/06/01 10:50:59 by vviovi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,8 @@ int	*get_line_texture(int pos, mlx_texture_t texture)
 	unsigned int	j;
 
 	line = malloc(sizeof(int) * texture.width * texture.bytes_per_pixel);
+	if (line == NULL)
+		return (NULL);
 	i = 0;
 	pos = pos * texture.bytes_per_pixel;
 	while (i < texture.width * texture.bytes_per_pixel)
@@ -32,16 +34,6 @@ int	*get_line_texture(int pos, mlx_texture_t texture)
 		i += texture.bytes_per_pixel;
 		pos += texture.width * texture.bytes_per_pixel;
 	}
-	//
-	//print verification
-	//
-	i = 0;
-	while (i < texture.width * texture.bytes_per_pixel)
-	{
-		ft_printf("line[i]: %d\n", line[i]);
-		i++;
-	}
-	ft_printf("====================================\n");
 	return (line);
 }
 
@@ -51,6 +43,8 @@ void	resize_texture(int *line_texture, int size, int scale_size)
 	int	i;
 
 	resized_line = malloc(sizeof(int) * scale_size);
+	if (resized_line == NULL)
+		return ;
 	i = 0;
 	while (i < scale_size)
 	{
@@ -59,4 +53,14 @@ void	resize_texture(int *line_texture, int size, int scale_size)
 	}
 	free(line_texture);
 	line_texture = resized_line;
+}
+
+int	scale_calculate(int dist, int size_texture)
+{
+	int	scale;
+
+	scale = size_texture / (dist / 10); //temporaire à voir qd on aura la 3d
+	if (scale > size_texture)
+		scale = size_texture;
+	return (scale);
 }
